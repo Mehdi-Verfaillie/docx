066e4f26cb95a118cea18a7b2b52fb9ba8eb8d35
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2023-09-07 16:45:04 +0200

    feat(tests): ensure all defined directories exist

--- src/test/suite/association.test.ts
+++ src/test/suite/association.test.ts
@@ -1,8 +1,22 @@
+import { expect } from 'chai'
 import * as sinon from 'sinon'
 import * as vscode from 'vscode'
 import { describe, setup, teardown, it } from 'mocha'
+import { JsonConfig, AssociationsManager } from '../../association'
 
 describe('Associations JSON Validation', () => {
+  const jsonMock = JSON.stringify({
+    associations: {
+      'src': ['/docx/ifTernary.md', '/docx/asyncAwait.md'],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'src/Controllers': ['/docx/controllers.md'],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'src/Modules': ['/docx/modules.md'],
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'src/Utils/dates.ts': ['/docx/utils/dates.md'],
+    },
+  })
+
   let statStub: sinon.SinonStub
   let manager: AssociationsManager
 
@@ -30,4 +44,15 @@ describe('Associations JSON Validation', () => {
   })
 
   teardown(() => statStub.restore())
+
+  it('should ensure all defined directories exist', async () => {
+    const jsonConfig = JSON.parse(jsonMock) as JsonConfig
+    const definedDirectories = Object.keys(jsonConfig.associations)
+
+    const results = await manager.directoriesExist(definedDirectories)
+
+    results.forEach((result) => {
+      expect(result.exists, `Directory ${result.directory} does not exist`).to.equal(true)
+    })
+  })
 })
