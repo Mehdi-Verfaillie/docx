799d9a958e76ebd75924fd85062ce44094c3d5b1
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2023-09-11 16:14:40 +0200

    feat: get file exist func

--- src/utils/files.utils.ts
+++ src/utils/files.utils.ts
@@ -18,4 +18,13 @@ export class FileManager {
       throw FileSystemError.FileNotFound(filePath)
     }
   }
+
+  public async getFileContent(filePath: string): Promise<string> {
+    try {
+      const fileUint8Array = await this.fs.readFile(Uri.file(filePath))
+      return new TextDecoder().decode(fileUint8Array)
+    } catch (error) {
+      throw new Error(`Failed to read file content: ${filePath}`)
+    }
+  }
 }
