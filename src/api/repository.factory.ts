3d2fe2dafc5f6c12c2d23dcd780f609110bbacef
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2023-09-12 18:00:58 +0200

    feat: create base repository factory struct

---
+++ src/api/repository.factory.ts
@@ -0,0 +1,26 @@
+import { Documentation } from '../association.manager'
+import { Provider, RepositoryProvider } from './repository.provider'
+import { ProviderConfig } from './repository.controller'
+
+export interface AbstractRepositoryFactory {
+  getDocumentations: () => Promise<Documentation[]>
+}
+
+export class RepositoryFactory implements AbstractRepositoryFactory {
+  private providers: Provider[] = []
+
+  constructor(configs: ProviderConfig[]) {
+    for (const config of configs) {
+      this.providers.push(new RepositoryProvider(config).instance)
+    }
+  }
+
+  public async getDocumentations(): Promise<Documentation[]> {
+    const allDocs: Documentation[] = []
+    for (const provider of this.providers) {
+      const docs = await provider.getDocumentations()
+      allDocs.push(...docs)
+    }
+    return allDocs
+  }
+}
