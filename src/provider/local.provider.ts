ae90191371e6cff7d6639b8912852295476ec9c6
Mehdi <mehdi@ublo.immo>
2023-09-15 10:41:52 +0200

    fix: update the function to find docs in nested directory

--- src/provider/local.provider.ts
+++ src/provider/local.provider.ts
@@ -1,7 +1,7 @@
 import { Documentation } from '../association.manager'
 import { WorkspaceManager } from '../utils/workspace.utils'
 import { FileSystemManager } from '../utils/fileSystem.utils'
-import { Uri } from 'vscode'
+import { Uri, FileType } from 'vscode'
 
 export interface ErrorHandler {
   code: string
@@ -20,20 +20,24 @@ export class LocalProvider {
 
   public async fetchDocumentation(directoryUri: Uri): Promise<Documentation[]> {
     const allFiles = (await this.fileSystem.readDirectory(directoryUri.fsPath)) ?? []
-    const documentationFiles = allFiles.filter(([filename]) =>
-      this.fileSystem.isFileOfInterest(filename)
-    )
+    let documentation: Documentation[] = []
 
-    const documentationPromises = documentationFiles.map(async ([filename]) => {
+    for (const [filename, fileType] of allFiles) {
       const filePath = Uri.joinPath(directoryUri, filename)
-      const content = await this.fileSystem.readFile(filePath.fsPath)
-      return {
-        name: filename,
-        type: this.fileSystem.getExtension(filename)!,
-        content: content,
-      }
-    })
 
-    return Promise.all(documentationPromises)
+      if (fileType === FileType.Directory) {
+        const nestedDocs = await this.fetchDocumentation(filePath)
+        documentation = documentation.concat(nestedDocs)
+      } else if (this.fileSystem.isFileOfInterest(filename)) {
+        const content = await this.fileSystem.readFile(filePath.fsPath)
+        documentation.push({
+          name: filename,
+          type: this.fileSystem.getExtension(filename)!,
+          content: content,
+        })
+      }
+    }
+
+    return documentation
   }
 }
