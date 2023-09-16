5b504de9586626c4b6818020f44ee61f20fc4ef3
Laura THAY <laura.thay@ipag.fr>
2023-09-16 17:14:23 +0200

    feat: open webview like md preview

--- src/webview/webview.ts
+++ src/webview/webview.ts
@@ -2,10 +2,24 @@ import * as vscode from 'vscode'
 import { Documentation } from '../association.manager'
 
 export function webView(file: Documentation) {
+  //create and show panel
   const panel = vscode.window.createWebviewPanel(file.type, file.name, vscode.ViewColumn.Two, {})
 
+  //add sets its HTML
   panel.webview.html = `
-    <pre>${file.content}</pre>
-   
-  `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Markdown Preview</title>
+    <style>
+      /* Votre CSS personnalisé peut être ajouté ici pour le rendu Markdown. */
+    </style>
+  </head>
+  <body>
+    ${file.content}
+    <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+  </body>
+  </html>
+`
 }
