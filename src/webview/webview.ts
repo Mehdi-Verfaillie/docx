85ad7e72b05d486f83b031f63810bf67a97393ea
Laura THAY <laura.thay@ipag.fr>
2023-09-19 20:25:00 +0200

    added comments

--- src/webview/webview.ts
+++ src/webview/webview.ts
@@ -2,10 +2,10 @@ import * as vscode from 'vscode'
 import { Documentation } from '../association.manager'
 
 export function webView(file: Documentation) {
-  //create and show panel
+  //create and show panel and show window on the right
   const panel = vscode.window.createWebviewPanel(file.type, file.name, vscode.ViewColumn.Two, {})
 
-  //add sets its HTML
+  //add sets its HTML shows like md preview
   panel.webview.html = `
   <!DOCTYPE html>
   <html lang="en">
