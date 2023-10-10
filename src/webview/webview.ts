9a56ee336587ef9e5129fe1dc6088e0e52078d03
Laura THAY <laura.thay@ipag.fr>
2023-10-10 20:29:20 +0200

    feat: added md anchor links

--- src/webview/webview.ts
+++ src/webview/webview.ts
@@ -1,16 +1,15 @@
 import * as vscode from 'vscode'
 import { Documentation } from '../association.manager'
+import markdownItAnchor from 'markdown-it-anchor'
 import markdownIt = require('markdown-it')
 
 export function webView(file: Documentation) {
   //create and show panel and show window on the right
   const panel = vscode.window.createWebviewPanel(file.type, file.name, vscode.ViewColumn.Two)
 
-  const md = markdownIt({
-    html: true,
-  })
-
-  const htmlContent = `
+  const md = markdownIt({ html: true }).use(markdownItAnchor)
+  //add sets its HTML shows like md preview
+  panel.webview.html = `
   <!DOCTYPE html>
   <html lang="en">
   <head>
@@ -20,9 +19,8 @@ export function webView(file: Documentation) {
       ${md.render(file.content)}
     </div>
   </body>
+  <script>
+</script>
   </html>
 `
-
-  //add sets its HTML shows like md preview
-  panel.webview.html = htmlContent
 }
